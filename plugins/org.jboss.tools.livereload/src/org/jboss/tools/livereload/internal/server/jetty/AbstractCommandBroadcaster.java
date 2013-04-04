c63d632d9099afd1c5bd7e3e1a1ce32500fb973e
Xavier Coulon <xcoulon@redhat.com>
2013-04-04 21:26:29 +0200

    JBIDE-13924 - Filter LiveReload messages on changed file extension

--- plugins/org.jboss.tools.livereload/src/org/jboss/tools/livereload/internal/server/jetty/AbstractCommandBroadcaster.java
+++ plugins/org.jboss.tools.livereload/src/org/jboss/tools/livereload/internal/server/jetty/AbstractCommandBroadcaster.java
@@ -13,7 +13,9 @@ package org.jboss.tools.livereload.internal.server.jetty;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IPath;
@@ -31,15 +33,20 @@ public abstract class AbstractCommandBroadcaster {
 
 	private final Set<LiveReloadSocket> connections = new HashSet<LiveReloadSocket>();
 
+	private static final List<String> acceptedFileTypes = Arrays.asList("html", "htm", "css", "js", "gif", "png",
+			"jpg", "jpeg", "bmp", "ico");
+
 	public void notifyResourceChange(final IPath path) {
 		try {
-			String cmd = buildRefreshCommand(path);
-			Logger.debug("Sending {} to {} client{}.", cmd, connections.size(), (connections.size() > 1 ? "s" : ""));
-			for (LiveReloadSocket connection : connections) {
-				if (connection.isOpen()) {
-					connection.sendMessage(cmd);
-				} else {
-					Logger.debug(" (removing a uncaught closed connection...)");
+			if (isRelevantFileType(path.getFileExtension())) {
+				String cmd = buildRefreshCommand(path);
+				Logger.debug("Sending {} to {} client{}.", cmd, connections.size(), (connections.size() > 1 ? "s" : ""));
+				for (LiveReloadSocket connection : connections) {
+					if (connection.isOpen()) {
+						connection.sendMessage(cmd);
+					} else {
+						Logger.debug(" (removing a uncaught closed connection...)");
+					}
 				}
 			}
 		} catch (Exception e) {
@@ -47,19 +54,23 @@ public abstract class AbstractCommandBroadcaster {
 		}
 	}
 
-	abstract String buildRefreshCommand(IPath path) throws JsonGenerationException, JsonMappingException,
-			IOException, URISyntaxException;
-	
+	private static boolean isRelevantFileType(final String fileExtension) {
+		return acceptedFileTypes.contains(fileExtension);
+	}
+
+	abstract String buildRefreshCommand(IPath path) throws JsonGenerationException, JsonMappingException, IOException,
+			URISyntaxException;
+
 	public void add(LiveReloadSocket liveReloadSocket) {
 		connections.add(liveReloadSocket);
-		Logger.debug("A new LiveReload client ({}) established a connection. Now serving {} client{}.", liveReloadSocket.getUserAgent(), connections.size(),
-				(connections.size() > 1 ? "s" : ""));
+		Logger.debug("A new LiveReload client ({}) established a connection. Now serving {} client{}.",
+				liveReloadSocket.getUserAgent(), connections.size(), (connections.size() > 1 ? "s" : ""));
 	}
 
 	public void remove(LiveReloadSocket liveReloadSocket) {
 		connections.remove(liveReloadSocket);
-		Logger.debug("A LiveReload client ({}) closed its connection. Now serving {} client{}.", liveReloadSocket.getUserAgent(), connections.size(),
-				(connections.size() > 1 ? "s" : ""));
+		Logger.debug("A LiveReload client ({}) closed its connection. Now serving {} client{}.",
+				liveReloadSocket.getUserAgent(), connections.size(), (connections.size() > 1 ? "s" : ""));
 
 	}
 
